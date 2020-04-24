f17cf3922feaf7290d1fba3bd3b1adee3310d96b
Robson Maia <robsonmvieira@gmail.com>
2020-04-24 07:39:06 -0300

    feat: create a index routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Main from '../pages/Main/index'
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Main} />
+  </Switch>
+)
+
+export default Routes
