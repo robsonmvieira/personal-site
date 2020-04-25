d296046c47e614bbd2ac1f807f0103a54784e56a
Robson Maia <robsonmvieira@gmail.com>
2020-04-25 18:56:36 -0300

    feat: add skill component in Main Page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Skills from '../../components/Skills'
 import Header from '../../components/Header'
+import About from '../../components/About'
 // import { Container } from './styles';
 
 const Main: React.FC = () => (
   <>
     <Header />
+    <About />
+    <Skills />
   </>
 )
 
