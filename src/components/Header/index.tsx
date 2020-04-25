9cf53e9db0626cc3deed5e81fffb84f41cb2916c
Robson Maia <robsonmvieira@gmail.com>
2020-04-25 17:34:44 -0300

    feat: update header to mobile screen

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,32 +1,53 @@
 /* eslint-disable no-undef */
 import React, { useState } from 'react'
 import { FiMenu } from 'react-icons/fi'
-import { Container, Nav } from './styles'
+import FbIcon from '../../assets/icons/facebook.svg'
+import TwIcon from '../../assets/icons/twitter.svg'
+import InstIcon from '../../assets/icons/instagram.svg'
+import LinkIcon from '../../assets/icons/linkedin.svg'
+import { Container, Wapper, Nav, List, Apresentation } from './styles'
 
 const Header: React.FC = () => {
-  ;[showMenu, setShowMenu] = useState(false)
-  const handleMenu = (): void => console.log('ok')
+  const [showMenu, setShowMenu] = useState(false)
+  const handleMenu = (): void => setShowMenu(!showMenu)
 
   return (
     <Container>
-      <Nav>
-        <a href="home">@RM</a>
-        <ul visible={showMenu}>
-          <li>
-            <a href="home">Home</a>
-          </li>
-          <li>
-            <a href="about">About</a>
-          </li>
-          <li>
-            <a href="a">Blog</a>
-          </li>
-          <li>
-            <a href="a">Contatc</a>
-          </li>
-        </ul>
+      <Wapper>
+        <Nav>
+          <a href="home">@RM</a>
+          <List visible={showMenu}>
+            <li>
+              <a href="home">Home</a>
+            </li>
+            <li>
+              <a href="about">About</a>
+            </li>
+            <li>
+              <a href="a">Blog</a>
+            </li>
+            <li>
+              <a href="a">Contatc</a>
+            </li>
+          </List>
+        </Nav>
         <FiMenu color="#fff" size={24} onClick={handleMenu} />
-      </Nav>
+      </Wapper>
+      <Apresentation>
+        <h1>I&#39;m Front-end Engineer &#38; Back-end developer</h1>
+        <h3>
+          Ideas transforms the world. in the next 20 minutes everythings can
+          changes. What&#39;s i can to do you today?
+        </h3>
+        <div>
+          <img src={LinkIcon} alt="" srcSet="" />
+          <img src={TwIcon} alt="" srcSet="" />
+          <img src={FbIcon} alt="" srcSet="" />
+          <img src={InstIcon} alt="" srcSet="" />
+        </div>
+
+        <button type="button">Contact Me Now</button>
+      </Apresentation>
     </Container>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,40 +1,40 @@
 import styled from 'styled-components'
+import Bg from '../../assets/images/bg.jpg'
+
+interface ListProperty {
+  visible: boolean
+}
 
 export const Container = styled.header`
+  @media only screen and (max-width: 480px) {
+    width: 100%;
+    background-image: url(${Bg});
+    overflow: hidden;
+    height: 500px;
+    background-size: cover;
+    position: relative;
+  }
+`
+export const Wapper = styled.div`
   width: 100%;
+  padding: 10px;
+  position: absolute;
   display: flex;
-  flex-direction: column;
+  background: #202430;
 `
 export const Nav = styled.nav`
   @media only screen and (max-width: 480px) {
     width: 100%;
-    background: #202430;
     display: flex;
-    /* align-items: center; */
-    justify-content: space-between;
-    padding: 10px;
-    margin: 0 auto;
+    flex-direction: column;
 
     > a {
       text-decoration: none;
       color: #fff;
       margin-top: 10px;
     }
-    ul {
-      display: flex;
-      flex-direction: column;
-      flex: 1;
-      margin-left: 140px;
-      li {
-        list-style: none;
-        padding: 10px;
-        a {
-          color: #fff;
-          text-decoration: none;
-        }
-      }
-    }
   }
+
   @media only screen and (min-width: 481px) and (max-width: 760px) {
     background: yellow;
     width: 100%;
@@ -42,5 +42,85 @@ export const Nav = styled.nav`
     display: flex;
     justify-content: space-between;
     align-items: center;
+  }
+`
+export const List = styled.ul<ListProperty>`
+  @media only screen and (max-width: 480px) {
+    display: ${(p: ListProperty) => (p.visible ? 'block' : 'none')};
+    margin-top: 10px;
+    flex-direction: column;
+    flex: 1;
+    li {
+      list-style: none;
+      margin-bottom: 10px;
+      a {
+        color: #fff;
+        text-decoration: none;
+        text-transform: uppercase;
+        font-family: 'Roboto', sans-serif;
+        font-size: 1rem;
+      }
+    }
+  }
+`
+export const Apresentation = styled.div`
+  @media only screen and (max-width: 480px) {
+    margin-top: 80px;
+    color: #fff;
+    padding: 10px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    flex-wrap: nowrap;
+
+    h1 {
+      font-family: 'Montserrat', sans-serif;
+      font-size: 2em;
+      line-height: 1.5;
+      width: 100%;
+      margin-bottom: 40px;
+      text-align: center;
+    }
+
+    h3 {
+      font-family: 'Montserrat', sans-serif;
+      font-size: 0.9em;
+      line-height: 1.5;
+      width: 100%;
+      padding: 24px 0 0 0;
+      text-align: center;
+    }
+    div {
+      width: 100%;
+      padding: 10px;
+      margin-top: 30px;
+      display: flex;
+
+      img {
+        cursor: pointer;
+      }
+      img:not(:last-of-type) {
+        margin-right: 100px;
+      }
+    }
+    button {
+      margin-top: 20px;
+      text-transform: uppercase;
+      border-radius: 10px;
+      padding: 15px 20px;
+      width: 200px;
+      border: 1px solid #fff;
+      background: transparent;
+      color: #fff;
+      opacity: 0.6;
+      transition: 0.3s;
+    }
+    button:hover {
+      background: #fff;
+      color: #202430;
+    }
+  }
+  @media only screen and (min-width: 481px) and (max-width: 760px) {
   }
 `
