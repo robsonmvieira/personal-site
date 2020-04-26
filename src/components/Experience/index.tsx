87aca9b9161d61e50f9b6bfad720e96c047271a7
Robson Maia <robsonmvieira@gmail.com>
2020-04-26 18:41:36 -0300

    feat: create Experience component

---
+++ src/components/Experience/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from './styles'
+
+const Experience: React.FC = () => (
+  <Container>
+    <h2>Experience</h2>
+  </Container>
+)
+
+export default Experience
