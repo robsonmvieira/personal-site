6a2fc39fcbcdeca278d8350d89fcbdbddf721f7e
Robson Maia <robsonmvieira@gmail.com>
2020-04-28 22:27:58 -0300

    feat: add XP component to Main Screen

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Skills from '../../components/Skills'
 import Header from '../../components/Header'
 import About from '../../components/About'
+import Experience from '../../components/Experience'
 // import { Container } from './styles';
 
 const Main: React.FC = () => (
@@ -9,6 +10,7 @@ const Main: React.FC = () => (
     <Header />
     <About />
     <Skills />
+    <Experience />
   </>
 )
 
